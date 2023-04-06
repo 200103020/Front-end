98b23d4f454067d0a7518ca606d4a594eb35e1ed
skadi <90984942+200103020@users.noreply.github.com>
2023-04-06 15:13:05 +0600

    all code

---
+++ bilboard_project/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+    
+    $('.cards').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPanding: '40px',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        cssEase: 'ease',
+        speed: 800,
+        infinite: true
+      });
+
+
+});
